536f56b14e7932be3036e025c7a406b96ad9edb4
Prashid710 <prashiddilshan0710@gmail.com>
2025-04-15 21:18:26 +0530

    edit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import homeImage from '../../images/hero-carousel-1.jpg';
+import About from '../About/About'
+import "./Home.css"
+import Menu from '../Menu/Menu';
+import Chefs from '../Chefs/Chefs';
+import Gallery from '../Gallery/Gallery';
+import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer'
+
+function Home() {
+  return (
+   <>
+
+<div className="image-container">
+      <img src={homeImage} alt="PD Restaurant" />
+
+      <div className="image-text">
+        Supper Delicious<br/> Foods in<br /> Sri Lanka!
+        <p>We do not cook, we create your emotions !</p>
+
+
+        <a><button className='booktablebtn'> Our Menu</button></a>
+        <a ><button className='menubtn'> Book a Table </button></a>
+      </div>
+      
+</div>
+
+
+<About></About>
+<Menu></Menu>
+<Chefs></Chefs>
+<Gallery></Gallery>
+<Contact></Contact>
+<Footer></Footer>
+
+
+   </>
+  )
+}
+
+export default Home
